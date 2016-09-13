edc99874d0b11ded8c3fa3c0c35a03da159a1f41
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2016-09-13 17:13:24 +0300

    begin payment

--- app/app_sitelette/scripts/views/rosterOrder/paymentView.js
+++ app/app_sitelette/scripts/views/rosterOrder/paymentView.js
@@ -1,12 +1,36 @@
 'use strict';
 
+var Vent = require('../../Vent'),
+    template = require('ejs!../../templates/rosterOrder/payment.ejs');
+
 var PaymentView = Backbone.View.extend({
 
 	name: 'payment',
 
+    id: 'cmtyx_payment',
+
 	initialize: function(options) {
-		debugger;
-	}
+		this.options = options || {};
+        this.on('show', this.onShow, this);
+        console.log(this.model.toJSON());
+        this.render();
+	},
+
+	render: function() {
+        this.$el.html(template(this.model.toJSON()));
+        this.setElement(this.$el.children().eq(0));
+        return this;
+    },
+
+    onShow: function() {
+        this.addEvents({
+            'click .next_btn': 'triggerPayment'
+        });
+    },
+
+    renderContent: function (options){
+        return this.$el;
+    }
 });
 
 module.exports = PaymentView;
