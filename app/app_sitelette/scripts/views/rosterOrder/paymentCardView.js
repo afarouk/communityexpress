297c2478177348a38dfeb28356d3d511e178e4b7
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2016-09-15 17:32:47 +0300

    card info

--- app/app_sitelette/scripts/views/rosterOrder/paymentCardView.js
+++ app/app_sitelette/scripts/views/rosterOrder/paymentCardView.js
@@ -20,28 +20,39 @@ var PaymentCardView = Backbone.View.extend({
         this.$el.html(template(this.renderData()));
         this.setElement(this.$el.children().eq(0));
 
-        this.prefillCard();
+        // this.prefillCard();
 
         return this;
     },
 
     prefillCard: function() {
         //TODO this
-        var cardData = this.model.get('creditCard'); 
+        var cardData = this.model.get('creditCard');
         this.card = new Skeuocard(this.$('#skeuocard'), {
+            //TODO doesn't work without number
             initialValues: {
-                number: '',//cardData.cardNumber,
+                number: '4111111111111111',//cardData.cardNumber,
                 expMonth: '' + cardData.expirationMonth,
                 expYear: '' + cardData.expirationYear,
                 name: cardData.firstName + ' ' + cardData.lastName,
                 cvc: cardData.cvv
-            }
+            },
+            validationState: {
+                // number: true,
+                // exp: true,
+                // name: true,
+                // cvc: true
+            },
+            debug: true
         });
+        // TODO try to set undefined when number was removed
+        this.$('[name="cc_number"]').val(undefined).trigger('change')
     },
 
     onShow: function() {
+        this.prefillCard();
         this.addEvents({
-            'click .nav_next_btn': 'triggerSummary',
+            'click .nav_next_btn': 'validateCard',
             'click .nav_back_btn': 'goBack'
         });
     },
@@ -56,7 +67,32 @@ var PaymentCardView = Backbone.View.extend({
     	});
     },
 
+    validateCard: function() {
+        var valid = this.card.isValid();
+        if (valid) {
+            this.setNewCardToModel();
+        }
+        this.triggerSummary();
+    },
+
+    setNewCardToModel: function() {
+        var cardData = this.model.get('creditCard'),
+            number = this.card._getUnderlyingValue('number'),
+            expMonth = this.card._getUnderlyingValue('expMonth'),
+            expYear = this.card._getUnderlyingValue('expYear'),
+            cvc = this.card._getUnderlyingValue('cvc'),
+            name = this.card._getUnderlyingValue('name'),
+            names = name.split(' ');
+        cardData.cardNumber = number,
+        cardData.expirationMonth = expMonth,
+        cardData.expirationYear = expYear,
+        cardData.firstName = names[0];
+        cardData.lastName = names[1];
+        cardData.cvv = cvc;
+    },
+
     triggerSummary: function() {
+
         Vent.trigger('viewChange', 'summary', {
             model: this.model,
             backTo: 'payment_card'
