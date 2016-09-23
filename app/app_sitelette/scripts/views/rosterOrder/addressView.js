699f0a260dfb91d690c39b1769f9dc162d3d9458
yurakravtsiv <yura.kravtsiv@gmail.com>
2016-09-23 12:37:13 +0300

    order and cart styles

--- app/app_sitelette/scripts/views/rosterOrder/addressView.js
+++ app/app_sitelette/scripts/views/rosterOrder/addressView.js
@@ -91,14 +91,18 @@ var AddressView = Backbone.View.extend({
 
     onDelivery: function() {
         this.$('.leftBtn').addClass('cmtyx_color_1');
+        this.$('.leftBtn').removeClass('cmtyx_text_color_1');
         this.$('.rightBtn').removeClass('cmtyx_color_1');
+        this.$('.rightBtn').addClass('cmtyx_text_color_1');
         this.model.set('pickupSelected', false);
         this.model.set('deliverySelected', true);
     },
 
     showMap: function() {
         this.$('.rightBtn').addClass('cmtyx_color_1');
+        this.$('.rightBtn').removeClass('cmtyx_text_color_1');
         this.$('.leftBtn').removeClass('cmtyx_color_1');
+        this.$('.leftBtn').addClass('cmtyx_text_color_1');
         this.model.set('pickupSelected', true);
         this.model.set('deliverySelected', false);
 
