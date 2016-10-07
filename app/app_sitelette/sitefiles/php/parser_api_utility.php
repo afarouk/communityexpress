4291e016e80c23cab40a1cdd282a81b7d87611d7
Alamgir Farouk <afarouk2@hotmail.com>
2016-10-07 16:57:14 -0700

    remove trailing carriage retur
    A trailing carriage return outside php block was starting the output.

--- app/app_sitelette/sitefiles/php/parser_api_utility.php
+++ app/app_sitelette/sitefiles/php/parser_api_utility.php
@@ -21,15 +21,15 @@ function php_log($param)
 {
     $file = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'php_console.log';
     //$file = 'php_console.log';
-    
-    // Write the contents to the file, 
+
+    // Write the contents to the file,
     // using the FILE_APPEND flag to append the content to the end of the file
     // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
     file_put_contents($file, $param . PHP_EOL, FILE_APPEND);
 }
 
 
-// validateParams function is responsible for validate and snitation of query parameter/query string 
+// validateParams function is responsible for validate and snitation of query parameter/query string
 //@param(string) is the key or name of variable used in url query string either post or get method
 function validateParams($param)
 {
@@ -54,12 +54,12 @@ function validateParams($param)
 
 function booleanToString($bool){
     if (is_bool($bool) === true) {
-        if($bool == true){ 
-            return "true";  
-        } else { 
+        if($bool == true){
+            return "true";
+        } else {
             return "false";
-        } 
-    } else { 
+        }
+    } else {
         return NULL;
     }
 }
@@ -68,40 +68,40 @@ function booleanToString($bool){
 // parameters - just complete http request including query string
 function makeApiCall($urlToCall)
 {
-    
+
     //
     $ch = curl_init();
-    
+
     //timeout after 10 seconds, you can increase it
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    
+
     //Set curl to return the data instead of printing it to the browser.
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, false);
-    
+
     // Some server may refuse your request if you dont pass user agent
     curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-    
+
     //not verifying ssl
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    
+
     //set the url, number of POST vars, POST data
-    
-    
+
+
     curl_setopt($ch, CURLOPT_URL, $urlToCall);
-    
+
     $curl_output = curl_exec($ch);
     if (curl_errno($ch)) {
         $data['curl_error'] = curl_error($ch);
-        
-        
+
+
     } else {
         $data               = json_decode($curl_output, true);
         $data['curl_error'] = false;
     }
-    
+
     return $data;
-    
+
 }
 
-?> 
+?>
