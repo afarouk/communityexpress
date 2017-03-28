eb984933cc2fa7456c6a5cb2272c8605fabf97f0
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2017-03-28 12:55:27 +0300

    reset to 8a5d103

--- app/app_sitelette/index.php
+++ app/app_sitelette/index.php
@@ -8,17 +8,13 @@ include_once('preprocessing.php');
 if (isset($pageAccess)) {
   include_once('sitefiles/pages/' . $pageAccess);
 } else if ($saslAccess || $urlKeyAccess) {
-  //if ($isDesktop) {
-  //  include_once('sitefiles/pages/' . 'common_desktop.php');
-//  } else {
+  if ($isDesktop) {
+    include_once('sitefiles/pages/' . 'common_desktop.php');
+  } else {
     if (!is_null($errorMessage)) {
       include_once('error_page/error_page.php');
     } else {
-      if ($isDesktop) {
-        include_once 'themes/'.$themeId.'/head.php';
-      }else{
-        include_once 'themes/'.$themeId.'/head_desktop.php';
-      }
+      include_once 'themes/'.$themeId.'/head.php';
       if ($useTemplate) {
         echo $siteletteJSON['landingViewHTML'];
       } else {
@@ -27,7 +23,7 @@ if (isset($pageAccess)) {
       echo '</html>';
     }
     /*end valid sitelette*/
-  //}
+  }
 } else {
   /*
    * neither sasl access or urlkey access.
