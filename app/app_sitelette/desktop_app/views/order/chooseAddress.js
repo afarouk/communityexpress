e12b73d8ec57291ef9edce26ef2d4ad56dff7a59
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2017-03-31 16:10:13 +0300

    address template data

--- app/app_sitelette/desktop_app/views/order/chooseAddress.js
+++ app/app_sitelette/desktop_app/views/order/chooseAddress.js
@@ -7,7 +7,30 @@ define([
 		template: template,
 		className: 'page choose_address_page',
 		initialize: function() {
-		}
+			console.log(this.model.additionalParams);
+			console.log(this.model.toJSON());
+		},
+		serializeData: function() {
+			var favorites = this.model.additionalParams.userModel.favorites,
+            	address = favorites.length !== 0 ? favorites.first().get('address') : this.getAddressFromSasl();
+			return _.extend(this.model.toJSON(), this.model.additionalParams, {
+				address: address
+			});
+		},
+
+		getAddressFromSasl: function() {
+	        var address = {
+	            name: saslData.saslName,
+	            street: saslData.street,
+	            street2: saslData.street2,
+	            number: saslData.number,
+	            city: saslData.city,
+	            state: saslData.state,
+	            zip: saslData.zip,
+	            phone: saslData.telephoneNumber
+	        };
+	        return address;
+	    },
 	});
 	return ChooseAddressView;
 });
