23e825597dc44f77eee2f68086f8e8fa42190308
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2017-04-24 14:47:06 +0300

    selections

--- app/app_sitelette/scripts/views/partials/customizationView.js
+++ app/app_sitelette/scripts/views/partials/customizationView.js
@@ -43,9 +43,66 @@ var CustomizationView = Backbone.View.extend({
             this.goBack();
             return;
         }
+        this.checkRadio();
         this.addEvents({
-            'click .back': 'goBack'
+            'click .back': 'goBack',
+            'change input': 'onSelected',
+            'click .done-btn': 'onDone'
         });
+    },
+
+    onSelected: function (e) {
+        var $target = $(e.currentTarget),
+            checked = $target.is(':checked'),
+            subId = $target.data('subid'),
+            subSubId = $target.data('subsubid'),
+            subItems = this.options.subItems;
+        var subItem = _.findWhere(subItems, {
+            subItemId: subId
+        });
+        if (subItem.selectorType.enumText === "CHECKBOX") {
+            var selected = this.$('input[type="checkbox"]:checked');
+            var noChecked = this.$('input:checkbox:not(:checked)');
+            var $row = noChecked.parents('.cmntyex-catalog-item');
+            var $subItem = noChecked.parents('[name="customize_subitem"]');
+            if (selected.length === subItem.maxSubSubCount) {
+                $row.addClass('not-allow');
+                $subItem.addClass('allow');
+            } else {
+                $row.removeClass('not-allow');
+                $subItem.removeClass('allow');
+            }
+        } else {
+            var selected = this.$('input[type="radio"]:checked');
+            var $subItem = selected.parents('[name="customize_subitem"]');
+            if (selected.length === subItem.maxSubSubCount) {
+                $subItem.addClass('allow');
+            } else {
+                $subItem.removeClass('allow');
+            }
+        }
+        this.changeSelectedState();
+    },
+
+    checkRadio: function() {
+        var selected = this.$('input[type="radio"]:checked');
+        var $subItem = selected.parents('[name="customize_subitem"]');
+        $subItem.addClass('allow');
+        this.changeSelectedState();
+    },
+
+    changeSelectedState: function() {
+        var $allowed = this.$('[name="customize_subitem"].allow'),
+            $doneBtns = this.$('.done-btn'); 
+        if ($allowed.length === this.options.subItems.length) {
+            $doneBtns.attr('disabled', false);
+        } else {
+            $doneBtns.attr('disabled', true);
+        }
+    },
+
+    onDone: function() {
+        debugger;
     },
 
     goBack: function() {
