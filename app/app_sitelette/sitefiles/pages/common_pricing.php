94bf2c49d16bff3afe64ea7ead704c8e72c727fc
Pradyut <pradyut.m@aqbsolutions.com>
2017-11-10 18:48:02 +0530

    old images renamed

---
+++ app/app_sitelette/sitefiles/pages/common_pricing.php
@@ -0,0 +1,28 @@
+<?php include_once ('sitefiles/php/detecturl.php')?>
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <?php
+include ('sitefiles/includes/stylesheets.html');
+  ?>
+  <title>Chalkboards-Signup</title>
+ </head>
+ <body data-spy="scroll" data-target=".navbar-fixed-top" >
+  <!-- Header start -->
+  <?php
+include ('sitefiles/includes/navbar.php');
+  ?>
+  <!-- Header end -->
+  <?php
+include ('sitefiles/pages/content_pricing.html');
+  ?>
+  <?php
+include ('sitefiles/includes/scriptfiles.html');
+  ?>
+ <script src="sitefiles/js/bootstrap-select.min.js"></script>
+  <?php
+include ('sitefiles/includes/footer.php');
+  ?>
+ </body>
+</html>
