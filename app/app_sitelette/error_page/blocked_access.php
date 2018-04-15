b2b7b0b26551ab04ea362274f0aebe083035e7a8
Alamgir Farouk <afarouk2@hotmail.com>
2018-04-15 16:45:37 -0700

    blocking access for known hack access

---
+++ app/app_sitelette/error_page/blocked_access.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<title>Chalkboards</title>
+
+		<link href="error_page/bootstrap.min.css" rel="stylesheet">
+		<link href="error_page/style.css" rel="stylesheet">
+
+		<script src="error_page/jquery.js"></script>
+		<script src="error_page/bootstrap.min.js"></script>
+		<script src="error_page/script.js"></script>
+
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h3> Blocked Access </h3>
+					<div class="error_message"> <?php echo $errorMessage ?>  </div>
+					<br>
+				 <a href="https://chalkboardstoday.com">chalkboardstoday.com</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
--- app/app_sitelette/index.php
+++ app/app_sitelette/index.php
@@ -3,39 +3,42 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 include_once('preprocessing.php');
-
-if ($saslAccess || $urlKeyAccess) {
-  if ($isDesktop) {
-    if (!is_null($errorMessage)) {
-      include_once('error_page/error_page.php');
-    } else {
-      include_once 'themes/'.$themeId.'/desktop/head.php';
-      if ($useTemplate) {
-        echo $siteletteJSON['landingViewHTML'];
-      } else {
-        include_once 'themes/'.$themeId.'/desktop/body.html';
-      }
-      echo '</html>';
-    }
-  } else {
-    if (!is_null($errorMessage)) {
-      include_once('error_page/error_page.php');
-    } else {
-      include_once 'themes/'.$themeId.'/mobile/head.php';
-      if ($useTemplate) {
-        echo $siteletteJSON['landingViewHTML'];
-      } else {
-        include_once 'themes/'.$themeId.'/mobile/body.html';
-      }
-      echo '</html>';
-    }
-  }
-} else if (isset($pageAccess)) {
-    include_once('sitefiles/pages/' . $pageAccess);
+if ($blockAccess) {
+  include_once('error_page/blocked_access.php');
 } else {
-  /*
-   * neither sasl access or urlkey access.
-   * neither URL nor sa,sl provided
-   */
-  include_once('sitefiles/pages/' . 'common_index.php');
+  if ($saslAccess || $urlKeyAccess) {
+    if ($isDesktop) {
+      if (!is_null($errorMessage)) {
+        include_once('error_page/error_page.php');
+      } else {
+        include_once 'themes/' . $themeId . '/desktop/head.php';
+        if ($useTemplate) {
+          echo $siteletteJSON['landingViewHTML'];
+        } else {
+          include_once 'themes/' . $themeId . '/desktop/body.html';
+        }
+        echo '</html>';
+      }
+    } else {
+      if (!is_null($errorMessage)) {
+        include_once('error_page/error_page.php');
+      } else {
+        include_once 'themes/' . $themeId . '/mobile/head.php';
+        if ($useTemplate) {
+          echo $siteletteJSON['landingViewHTML'];
+        } else {
+          include_once 'themes/' . $themeId . '/mobile/body.html';
+        }
+        echo '</html>';
+      }
+    }
+  } else if (isset($pageAccess)) {
+    include_once('sitefiles/pages/' . $pageAccess);
+  } else {
+    /*
+     * neither sasl access or urlkey access.
+     * neither URL nor sa,sl provided
+     */
+    include_once('sitefiles/pages/' . 'common_index.php');
+  }
 }
