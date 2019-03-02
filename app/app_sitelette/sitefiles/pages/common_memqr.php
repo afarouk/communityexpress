dedfd70f398d3d5058514f6b18736053b02646f5
Alamgir Farouk <afarouk2@hotmail.com>
2019-03-02 13:43:13 -0800

    more memqr

--- app/app_sitelette/sitefiles/pages/common_memqr.php
+++ app/app_sitelette/sitefiles/pages/common_memqr.php
@@ -6,7 +6,19 @@
 <?php
 include('sitefiles/includes/stylesheets.html');
 ?>
-<title>Chalkboards-Members</title>
+<title>Chalkboards</title>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+
+<link rel="apple-touch-startup-image" href="">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
+<link rel="apple-touch-icon"  href=" ">
+<link rel="icon" sizes="192x192" href=" ">
+
 </head>
 <body data-spy="scroll" data-target=".navbar-fixed-top" >
 <!-- Header start -->
--- app/app_sitelette/sitefiles/pages_js/common_memqr.js
+++ app/app_sitelette/sitefiles/pages_js/common_memqr.js
@@ -1,24 +1,26 @@
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
+
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
+
 function eraseCookie(name) {
-    document.cookie = name+'=; Max-Age=-99999999;';
+  document.cookie = name + '=; Max-Age=-99999999;';
 }
 
 
@@ -38,16 +40,16 @@ $(document).ready(
       alert("iid=" + window.communityRequestProfile.iid);
     });
     */
-    
+
     /* resolve iid to uid */
 
     console.log("Server: " + window.communityRequestProfile.api_server);
 
-    var restURL = "http://"+window.communityRequestProfile.api_server + "/apptsvc/rest/authentication/retrieveOrCreateUserByKUID"
+    var restURL = window.communityRequestProfile.protocol + window.communityRequestProfile.api_server + "/apptsvc/rest/authentication/retrieveOrCreateUserByKUID"
     console.log(restURL);
 
     var postData = JSON.stringify({
-      kUID:window.communityRequestProfile.iid
+      kUID: window.communityRequestProfile.iid
     });
 
     $.ajax({
@@ -59,34 +61,46 @@ $(document).ready(
       console.log("userRegistrationDetails :" + userRegistrationDetails);
       if (typeof userRegistrationDetails.uid !== 'undefined') {
 
-          /*
-          * save it in localstorage
-          *
-          */
-          console.log(" saving to local storage cmxUID:" +
-              userRegistrationDetails.uid);
-          localStorage.setItem("cmxUID", userRegistrationDetails.uid);
-          setCookie('cmxUID',userRegistrationDetails.uid,365);
-          //Cookies.set('cmxUID',userRegistrationDetails.uid , {expires:365});
-          //this.setUser(userRegistrationDetails.uid, userRegistrationDetails.userName);
+        /*
+         * save it in localstorage
+         *
+         */
+        console.log(" saving to local storage cmxUID:" +
+          userRegistrationDetails.uid);
+        localStorage.setItem("cmxUID", userRegistrationDetails.uid);
+        setCookie('cmxUID', userRegistrationDetails.uid, 365);
+
+
+        var boolCookie = (userRegistrationDetails.adhocEntry ? 'true' : 'false');
+        console.log(" saving to local storage cmxAdhocEntry:" +
+          boolCookie);
+        setCookie('cmxAdhocEntry', boolCookie, 365);
+        //Cookies.set('cmxUID',userRegistrationDetails.uid , {expires:365});
+        //this.setUser(userRegistrationDetails.uid, userRegistrationDetails.userName);
+
+        /* echo check */
+        var adhocEntry;
+        var adhocEntryCookie = getCookie('cmxAdhocEntry');
+        if (adhocEntryCookie === 'true') {
+          adhocEntry = true;
+        } else {
+          adhocEntry = false;
+        }
+        console.log("Echo check: adhocEntry:"+adhocEntry);
+
       }
 
     }).fail(function(jqXHR, textStatus, errorThrown) {
       var message = processAjaxError(jqXHR);
       var success = false;
-      console.log("Error:"+message);
+      console.log("Error:" + message);
       /*
        *
        */
     }).always(function() {
       /* ladda_submit_mobile_button.stop(); */
     });
-
-
-
-
-
-
+ 
 
 
   });
