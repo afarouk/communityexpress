347d46bca45e41ff11ac450eaf74ea83fa8d58b5
Max <max.ruchkin@gmail.com>
2020-09-07 19:26:08 +0300

    manifest fix

--- app/app_sitelette/manifest.php
+++ app/app_sitelette/manifest.php
@@ -6,10 +6,11 @@ ini_set('display_errors', 1);
 /*React App changes*/
 /*Handle request to /manifest.json to return dynamically modified file for a specified sasl*/
 $manifest = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'youdash' . DIRECTORY_SEPARATOR . 'manifest.json', true));
-$friendlyURL = $_GET['friendlyURL'];
-if ($friendlyURL) {
+$url = $_GET['url'];
+//print_r($_GET);
+if ($url) {
     $demo = $_GET['demo'];
-    $manifest->start_url = $friendlyURL . ($demo ? '/?demo=true' : '') ;
+    $manifest->start_url = $url . ($demo ? '/?demo=true' : '') ;
     header('Content-type: application/json');
     echo json_encode($manifest, JSON_UNESCAPED_SLASHES);
     exit();
